1e41eb5e0cdd0637d0d3eab21e023dbeea77325d
jennywy <jennywy@gmail.com>
2017-09-22 14:23:24 -0400

    Add macros to models
    Added macros to User and Language model so they can talk to each other

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,4 @@
 class Language < ApplicationRecord
+  has_many :userlanguages
+  has_many :users, through: :userlanguages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@
 class User < ApplicationRecord
   include Authentication
   has_many :examples
+  has_many :languages, through: :userlanguages
+  has_many :userlanguages
 end
