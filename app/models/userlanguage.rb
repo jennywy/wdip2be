190419365a84da73b0a00de942a7dda1832911a3
jennywy <jennywy@gmail.com>
2017-09-27 12:37:50 -0400

    Validates Uniqueness of language id per user id

--- app/models/userlanguage.rb
+++ app/models/userlanguage.rb
@@ -1,4 +1,5 @@
 class Userlanguage < ApplicationRecord
   belongs_to :user
   belongs_to :language
+  validates_uniqueness_of :language_id, scope: :user_id, :message => "You may already have this language. Please update instead"
 end
